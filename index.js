badab226522b31ba712ba0c58ddf937916230be1
Titan <139656473+Titanx940@users.noreply.github.com>
2023-10-17 19:29:08 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,12 +1,41 @@
-const fs = require("fs");
+import express from "express";
+import bodyParser from "body-parser";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
-// fs.writeFile("message.txt", "Hello Node", (err) => {
-//   if (err) throw err;
-//   console.log("The file has been saved!");
-// });
+const app = express();
+const port = 3000;
 
-fs.readFile("./message.txt", "utf8", (err, data) => {
-  if (err) throw err;
-  console.log(data);
+let userIsAuthorised = false;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+function passwordCheck(req, res, next) {
+  const password = req.body["password"];
+  if (password === "ILoveProgramming") {
+    userIsAuthorised = true;
+  } else {
+    userIsAuthorised = false;
+  }
+  next();
+}
+
+app.use(passwordCheck);
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
 });
 
+app.post("/check", (req, res) => {
+  if (userIsAuthorised) {
+    res.sendFile(__dirname + "/public/secret.html");
+  } else {
+    res.redirect("/");
+    //Or- res.sendFile(__dirname + "/public/index.html");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Listening on http://localhost:${port}`);
+});
